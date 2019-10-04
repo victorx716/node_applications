97a0a9712879b70c184cac1c9fc521f19dbd095b
Victor Liu <vliu@ibm.com>
2019-10-04 10:56:05 -0500

    Set up dynamic pages with handlebars templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,8 +3,28 @@ const path = require('path')
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'hbs')
 app.use(express.static(path.join(__dirname, '../public')))
 
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Vic Liu'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me',
+    name: 'Kyle'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help page'
+  })
+})
 const port = process.env.port || 3000;
 
 app.get('/weather', (req, res) => {
