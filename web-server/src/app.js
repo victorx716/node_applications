9fb092654552c43d45a10cde60b2a8ef1a565df6
Victor Liu <vliu@ibm.com>
2019-10-04 18:06:39 -0500

    Setup all endpoints of weather app and finish styling

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,28 +1,42 @@
 const path = require('path')
-
 const express = require('express')
+const hbs = require('hbs')
 const app = express()
 
+// Define the paths for Express.js config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Set up Handlebars.js engine and views location
 app.set('view engine', 'hbs')
-app.use(express.static(path.join(__dirname, '../public')))
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
   res.render('index', {
     title: 'Weather App',
-    name: 'Vic Liu'
+    name: 'Vic Liu',
+    footer: 'created by vicNYC'
   })
 })
 
 app.get('/about', (req, res) => {
   res.render('about', {
     title: 'About Me',
-    name: 'Kyle'
+    name: 'Kyle',
+    footer: 'about by vicNYC'
   })
 })
 
 app.get('/help', (req, res) => {
   res.render('help', {
-    title: 'Help page'
+    title: 'Help Page',
+    name: 'Vic',
+    footer: 'help by vicNYC'
   })
 })
 const port = process.env.port || 3000;
@@ -30,10 +44,24 @@ const port = process.env.port || 3000;
 app.get('/weather', (req, res) => {
   res.send({
     forecast: 'Sunny with a chance of meatballs',
-    location: 'Brooklyn, New York'
+    location: 'Brooklyn, New York',
+    footer: 'weather by vicNYC'
   })
 })
 
+app.get('/help/*', (req, res) => {
+  res.render('error', {
+    error: "we couldn't find the help article :(",
+    title: '404'
+  })
+})
+
+app.get('*', (req, res) => {
+  res.render('error', {
+    error: "Sorry about that- we couldn't find the page you were looking for",
+    title: '404'
+  })
+})
 
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
