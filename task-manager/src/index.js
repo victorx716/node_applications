7de9d1f23ebd928923de8fcdbe21ff375f739eaa
Victor Liu <vliu@ibm.com>
2019-10-09 17:48:42 -0500

    Include custom middleware to intercept all route handlers

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -6,9 +6,24 @@ const userRouter = require('./routers/user')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+  // if (req.method === 'GET') {
+  //   res.send('GET requests are disabled')
+  // } else {
+  //   next()
+  // }
+// })
+
+// custom middleware
+app.use((req, res, next) => {
+  res.status(503).send('Site is currently in maintenance. Please try again later')
+})
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
+
+
 
 app.listen(port, () => {
   console.log('Server is listening on port ' + port)
--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -4,9 +4,11 @@ const router = new express.Router()
 
 router.post('/users', async (req, res) => {
   const user = new User(req.body)
+
   try {
     await user.save()
-    res.status(201).send(user)
+    const token = await user.generateAuthToken()
+    res.status(201).send({ user, token })
   } catch (e) {
     res.status(400).send(e)
   }
