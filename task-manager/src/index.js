566b6e644f926cffac9fe900f73ac41e7c000a5e
Victor Liu <vliu@ibm.com>
2019-10-11 12:30:42 -0500

    Add more tests for restful services and authentication

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,14 +1,6 @@
-require('./db/mongoose')
-const express = require('express')
-const taskRouter = require('./routers/task')
-const userRouter = require('./routers/user')
+const app = require('./app')
 
-const app = express()
 const port = process.env.PORT
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
 
 app.listen(port, () => {
   console.log('Server is listening on port ' + port)
