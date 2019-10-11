842e6e886cddc875fa565d877e2ceed30ee5ad45
Victor Liu <victorliu716@gmail.com>
2019-10-11 01:26:31 -0500

    Add asynchronous test example

--- task-manager/src/math.js
+++ task-manager/src/math.js
@@ -18,10 +18,23 @@ const celsiusToFahrenheit = (temp) => {
 // 2. Create "Should convert 32 F to 0 C"
 // 3. Create "Should convert 0 C to 32 F"
 // 4. Run the Jest to test your work!
+const add = (a, b) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+
+      if (a < 0  || b < 0) {
+        return reject('Numbers must be positive')
+      }
+      resolve(a + b)
+    }, 2000)
+  })
+}
+
 
 
 module.exports = {
   calculateTip,
   fahrenheitToCelsius,
-  celsiusToFahrenheit
+  celsiusToFahrenheit,
+  add
 }
