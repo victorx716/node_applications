c17087d2e00b470a4da37f3a8f48833fb1bab7f0
Victor Liu <victorliu716@gmail.com>
2019-10-13 15:35:32 -0500

    Setup event acknowledgement for chat app

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -8,3 +8,30 @@ socket.on('message', (message) => {
 //   console.log('Clicked')
 //   socket.emit('increment')
 // })
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const message = e.target.elements.message.value
+
+  socket.emit('sendMessage', message, (error) => {
+    if (error) {
+      return console.log(error)
+    }
+    console.log('msg delivered')
+  })
+})
+
+document.querySelector('#send-location').addEventListener('click', () => {
+   if (!navigator.geolocation) {
+     return alert('geolocation not supported')
+   }
+
+   navigator.geolocation.getCurrentPosition((position) => {
+    socket.emit('sendLocation', {latitude: position.coords.latitude, longitude: position.coords.longitude}, (err) => {
+      if (err) {
+        return console.log(err)
+      }
+      console.log('loc delivered')
+    })
+   })
+})
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -5,19 +5,35 @@ const http = require('http')
 const server = http.createServer(app)
 const socketio = require('socket.io')
 const io = socketio(server)
+const Filter = require('bad-words')
 
 const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, '../public')))
 
-let count = 0
-
 io.on('connection', (socket) => {
 
   socket.emit('message', 'welcome!')
 
-  socket.on('message', () => {
-    io.emit('countUpdated', 'Welcome!')
+  socket.broadcast.emit('message', 'a new user joined')
+
+  socket.on('sendMessage', (message, cb) => {
+    const filter = new Filter()
+
+    if (filter.isProfane(message)) {
+      return cb('profanity detected')
+    }
+    io.emit('message', message)
+    cb()
+  })
+
+  socket.on('disconnect', () => {
+    io.emit('message', 'user has left the chat')
+  })
+
+  socket.on('sendLocation', (location, cb) => {
+    io.emit('message', `https://google.com/maps?q=${location.latitude},${location.longitude}` )
+    cb()
   })
 })
 
